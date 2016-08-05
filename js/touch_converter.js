a115d0a14c234329de687f514ad031e5204d22d9
Thomas Chapeaux <thomas.chapeaux@sabca.be>
2016-08-05 14:08:48 +0200

    Add hacked version of touch support

---
+++ js/touch_converter.js
@@ -0,0 +1,26 @@
+// touch screen functionality converter : translates "touch" events to Button press
+function convertTouchEvent(ev) {
+    var touch, ev_type, mouse_ev;
+    touch = ev.targetTouches[0];
+    ev.preventDefault();
+    switch (ev.type) {
+    case 'touchstart':
+        keyDown(66);
+        break;
+    case 'touchend':
+        // Make sure only one finger is lifted from the target
+        // TODO AND CHECK: check that targetTouches is empty?
+        if (ev.changedTouches.length != 1) {
+            return;
+        }
+        keyUp(66);
+        break;
+    default:
+        return;
+    }
+};
+
+function touch2mouse(el) {
+    el.addEventListener("touchstart", convertTouchEvent);
+    el.addEventListener("touchend", convertTouchEvent);
+};
